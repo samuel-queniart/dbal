22d44d0a3cab97856c82b837c3ee9cb182006369
Simon Podlipsky <simon@podlipsky.net>
2017-12-26 00:56:20 +0100

    Fix getting portable sequence definition for PSQL10

--- tests/Doctrine/Tests/DBAL/Functional/Schema/OracleSchemaManagerTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/Schema/OracleSchemaManagerTest.php
@@ -273,4 +273,9 @@ class OracleSchemaManagerTest extends SchemaManagerFunctionalTestCase
         $this->assertSame('datetime', $columns['col_datetime']->getType()->getName());
         $this->assertSame('datetimetz', $columns['col_datetimetz']->getType()->getName());
     }
+
+    public function testCreateAndListSequences() : void
+    {
+        self::markTestSkipped("Skipped for uppercase letters are contained in sequences' names. Fix the schema manager in 3.0.");
+    }
 }
